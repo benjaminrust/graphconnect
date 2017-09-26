93a59acffc84a82ce30d47dc8830608294e8fe2d
gaskaj <joegaska@gmail.com>
2017-09-26 14:59:40 -0400

    Modified req

--- app/utils/ForceUtil.scala
+++ app/utils/ForceUtil.scala
@@ -145,7 +145,7 @@ class ForceUtil @Inject() (configuration: Configuration, ws: WSClient) (implicit
     }
   }
 
-  def processHistory(env: String, sessionId: String, body: String): Future[JsValue] = {
+  def processHistory(env: String, sessionId: String, body: String): Future[Seq[JsObject]] = {
       restUrl(env, sessionId).flatMap { restUrl =>
         ws(restUrl + "tooling/executeAnonymous", sessionId).withQueryString("anonymousBody" -> body).get().flatMap { response =>
             response.status match {
