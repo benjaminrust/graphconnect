831532906cf6b7b2061122ff9bb1336fc30f420d
gaskaj <joegaska@gmail.com>
2017-09-26 15:22:40 -0400

    Added Success Logic

--- app/utils/ForceUtil.scala
+++ app/utils/ForceUtil.scala
@@ -149,7 +149,7 @@ class ForceUtil @Inject() (configuration: Configuration, ws: WSClient) (implicit
       restUrl(env, sessionId).flatMap { restUrl =>
         ws(restUrl + "tooling/executeAnonymous", sessionId).withQueryString("anonymousBody" -> body).get().flatMap { response =>
             response.status match {
-                case Status.OK => Future.successful(response.body.as[Seq[JsObject]])
+                case Status.OK => Future.successful("Successfully Submitted History for Processing")
                 case _ => Future.failed(new Exception(response.body))
             }
          }
