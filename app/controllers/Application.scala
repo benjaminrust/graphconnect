61d5fd18bb6a871c88fdb2e5a46a5dd9dfa007bf
gaskaj <joegaska@gmail.com>
2017-09-27 08:08:33 -0400

    Updated Process History

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -109,8 +109,6 @@ class Application @Inject() (forceUtil: ForceUtil, ws: WSClient, configuration: 
 
         triggerTestCreate <- forceUtil.createApexClass(request.env, request.sessionId, triggerMetadata.name, triggerTestBody)
 
-        processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');")
-
       } yield (webhookCreate, remoteSiteSettingCreate, triggerCreate, triggerTestCreate)
 
       webhookCreateFuture.map(_ => Ok(Results.EmptyContent())).recover {
