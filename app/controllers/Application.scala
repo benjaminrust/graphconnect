bece9bd7abc988ab608ee95df7f70000b6fcb4e6
gaskaj <joegaska@gmail.com>
2017-09-28 10:25:49 -0400

    Moved history up

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -101,6 +101,8 @@ class Application @Inject() (forceUtil: ForceUtil, ws: WSClient, configuration: 
 
         remoteSiteSettingCreate <- forceUtil.createRemoteSite(request.env, request.sessionId, triggerMetadata.name + "RemoteSiteSetting", triggerMetadata.url)
 
+        processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');")
+
         triggerBody = apextemplates.triggers.txt.WebhookTrigger(triggerMetadata.name, triggerMetadata.sobject, triggerMetadata.events.map(_.toString), triggerMetadata.url).body
 
         triggerCreate <- forceUtil.createApexTrigger(request.env, request.sessionId, triggerMetadata.name, triggerBody, triggerMetadata.sobject)
@@ -108,8 +110,6 @@ class Application @Inject() (forceUtil: ForceUtil, ws: WSClient, configuration: 
         triggerTestBody = apextemplates.classes.txt.TriggerTest(triggerMetadata.name, triggerMetadata.sobject, triggerMetadata.events.map(_.toString), triggerMetadata.url).body
 
         triggerTestCreate <- forceUtil.createApexClass(request.env, request.sessionId, triggerMetadata.name, triggerTestBody)
-
-        processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');")
 
         //processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');").recover {
         //    // Ignore everything
