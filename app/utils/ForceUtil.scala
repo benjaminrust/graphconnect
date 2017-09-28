5c78b6031c2cee27f9abbc071fbf359df63ac555
gaskaj <joegaska@gmail.com>
2017-09-28 10:18:23 -0400

    Default Successful

--- app/utils/ForceUtil.scala
+++ app/utils/ForceUtil.scala
@@ -149,7 +149,7 @@ class ForceUtil @Inject() (configuration: Configuration, ws: WSClient) (implicit
     restUrl(env, sessionId).flatMap { restUrl =>
         ws(restUrl + "tooling/executeAnonymous", sessionId).withQueryString("anonymousBody" -> body).get().flatMap { response =>
             response.status match {
-                case _ => Seq[JsObject]
+                case _ => Future.successful(response.json)
             }
         }
     }
