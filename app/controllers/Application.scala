4611b0b7b0a57e135f8f474ed8072509bee5d5e1
gaskaj <joegaska@gmail.com>
2017-09-28 10:30:45 -0400

    Added Exception Handling

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -101,8 +101,6 @@ class Application @Inject() (forceUtil: ForceUtil, ws: WSClient, configuration: 
 
         remoteSiteSettingCreate <- forceUtil.createRemoteSite(request.env, request.sessionId, triggerMetadata.name + "RemoteSiteSetting", triggerMetadata.url)
 
-        processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');")
-
         triggerBody = apextemplates.triggers.txt.WebhookTrigger(triggerMetadata.name, triggerMetadata.sobject, triggerMetadata.events.map(_.toString), triggerMetadata.url).body
 
         triggerCreate <- forceUtil.createApexTrigger(request.env, request.sessionId, triggerMetadata.name, triggerBody, triggerMetadata.sobject)
@@ -111,10 +109,9 @@ class Application @Inject() (forceUtil: ForceUtil, ws: WSClient, configuration: 
 
         triggerTestCreate <- forceUtil.createApexClass(request.env, request.sessionId, triggerMetadata.name, triggerTestBody)
 
-        //processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');").recover {
-        //    // Ignore everything
-        //    case _: val newJSON = Json.obj()
-        //}
+        processHistory <- forceUtil.processHistory(request.env, request.sessionId,"HardingPointBatch.ProcessHistory('" + triggerMetadata.sobject + "');").recover {
+            case e: forceUtil.DuplicateException => Json.obj()
+        }
 
       } yield (webhookCreate, remoteSiteSettingCreate, triggerCreate, triggerTestCreate)
 
